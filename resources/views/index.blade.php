e0842c764ed861fc3b074003afb88a61ddec5031
r1zkyal <alfiansyahrizky968@gmai.com>
2024-06-23 21:29:12 +0700

    commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -49,8 +49,10 @@
                 </tr>
                 @endforeach
             </table>
+            <div>
+                {{ $pasiens->links() }}
+            </div>
         </div>
-
     </div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
